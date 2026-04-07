ef679fabf1ed7af4e41d939cc02d6ab770da54db
Denay2468 <denayaung13@gmail.com>
2026-04-07 09:46:41 +0630

    do while and code

--- loop.cpp
+++ loop.cpp
@@ -24,3 +24,12 @@ while (x = 5){
 
 cout<<"the last random while number ="<<x<<endl;
 cout<<endl;
+
+cout<<"Loop do while "<<endl;
+x = 1 + rand() % 10;
+do {
+    cout<<"random number: "<<x<<endl;
+    x = 1 + rand() % 10;
+} while (x <= 5);
+cout<<"the last random do-while number ="<<x<<endl;
+cout<<endl;

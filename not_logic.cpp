49ceeb7868cb713fa3e66289560a4b537f10783e
Denay2468 <denayaung13@gmail.com>
2026-04-07 09:54:33 +0630

    create main function and code

---
+++ not_logic.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float ScoreA,ScoreB,Avarage;
+    string status;
+
+    cout<<"Enter Indonesian Language Score : ";
+    cin>>ScoreB;
+    cout<<"Enter English Score : ";
+    cin>>ScoreA;
+}

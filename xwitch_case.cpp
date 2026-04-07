2b89d970831a4ddd710912de812b804660b6cc1d
Denay2468 <denayaung13@gmail.com>
2026-04-07 10:19:37 +0630

    create input function and code

--- xwitch_case.cpp
+++ xwitch_case.cpp
@@ -4,4 +4,14 @@ using namespace std;
 int departmentcode;
 string departmentname,quotastatus;
 
+void input(){
+    cout<<"=== Campus major registration ==="<<endl;
+    cout<<"1. Information Technology"<<endl;
+    cout<<"2. Electrical Engineering"<<endl;
+    cout<<"3. System Information"<<endl;
+    cout<<"========================"<<endl;
+    
+    cout<<"Enter your choice (1-3): ";
+    cin>>departmentcode;
 
+}
